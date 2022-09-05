cd47697352a3784bd12468e000462f3cbb60f974
Ognjen Raketic <110744188+Raketic-Ognjen@users.noreply.github.com>
2022-09-05 21:44:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm? \n"))
 if height > 120:
   print("You can ride the rolercoster")
+  age = int(input("What is your age?\n"))
+  if  age <= 18:
+    print("Your ticket price is 7$")
+  else:
+    print("Your ticket price is 12$")
 else:
   print("Can ride the rolercoster")
